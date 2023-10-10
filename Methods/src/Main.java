ed714d36cb9ec3d09e91d0fbec77c510137a8928
junhyung <wnsgud0895@gmail.com>
2023-10-10 16:50:14 +0900

    .

--- Methods/src/Main.java
+++ Methods/src/Main.java
@@ -4,11 +4,29 @@ public class Main {
 	public static void main(String[] args) {
 		
 		// method = a block of code that is executed whenever it is called upon
-		hello();
+//		hello();
+		
+		String name = "Bro";
+		int age = 21;
+		
+		hello(name, age);
+		
+		int x = 3;
+		int y = 4;
+		
+		int z = add(x, y);
+		System.out.println(z);
 	}
 	
-	static void hello() {
-		System.out.println("Hello");
+	static void hello(String name, int age) {
+		System.out.println("Hello "+name);
+		System.out.println("You are "+age);
+	}
+	
+	static int add(int x, int y) {
+		
+		int z = x + y;
+		return z;
 	}
 
 }
