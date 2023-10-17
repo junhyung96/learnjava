381e1d910f16cd9d1f38fe75ee8cd2e067837d2e
junhyung <wnsgud0895@gmail.com>
2023-10-17 14:23:29 +0900

    1

---
+++ baekjoon/src/baekjoon/BOJ12919.java
@@ -0,0 +1,23 @@
+package baekjoon;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class BOJ12919 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+		String S = buffer.readLine();
+		String T = buffer.readLine();
+		
+		System.out.println(S);
+		System.out.println(T);
+		
+		make_string(S, T);
+	}
+	
+	public static void make_string(String s, String T) throws IOException {
+		
+		
+	}
+}
